837213b5b9f1d6232ca059540396269785c32563
picasso250 <cumt.xiaochi@gmail.com>
2014-01-24 00:18:57 +0800

    db struct has_visit

--- install.sql
+++ install.sql
@@ -7,13 +7,16 @@ CREATE TABLE `relation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
 
+delimiter $$
 
 CREATE TABLE `student` (
-  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `school_name` varchar(45) DEFAULT NULL,
   `star` tinyint(4) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
+  `has_visit` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `is_private` tinyint(3) unsigned DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
