1bb64df8b13bf68916dfde21926409e6aa9efd7c
xiaochi <cumt.xiaochi@gmail.com>
2014-01-23 20:04:04 +0800

    return dict

--- parser.py
+++ parser.py
@@ -11,32 +11,25 @@ import urllib
 from urlparse import urlparse
 
 def get_friend_list(content):
-    pass
     d = pq(content)
-    # print d
     li_list = d('#friendListCon > li')
-    # li_list = ol.find('li')
     for li in li_list:
-        pass
-        # a_list = li.find('a')
-        # if a_list is not None:
-        #     for a in a_list:
-        #         print a.text
-        # print dir(li)
-        print '------------------'
         img = li.find('p').find('a').find('img')
         avatar = img.attrib['src']
-        print 'avatar', avatar
+        print 'avatar:', avatar
         a = li.find('div').find('dl').find('dd').find('a')
         name = a.text
-        print 'name', name
+        print 'name:', name
         url = a.attrib['href']
         m = re.search('id=(\d+)', url)
         uid = m.group(1)
-        print 'uid', uid
+        print 'uid:', uid
+        return {
+            avatar: avatar,
+            name: name,
+            uid: uid,
+        }
         # todo get city or school
-        # todo page
-        # todo can next page?
         # todo save to mysql
 
 def get_page_count(content):
