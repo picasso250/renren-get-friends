cf0ec796172f44c3718e45d652f6821420d8b122
picasso250 <cumt.xiaochi@gmail.com>
2014-01-23 22:47:08 +0800

    insert

--- db.py
+++ db.py
@@ -1,12 +1,43 @@
 import MySQLdb
 
-try:
-    conn=MySQLdb.connect(host='localhost',user='root',passwd='',db='renren_data',port=3306)
-    cur=conn.cursor()
-    cur.execute('select * from test')
-    a = cur.fetchmany()
-    print a[0][0]
-    cur.close()
-    conn.close()
-except MySQLdb.Error,e:
-     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+# try:
+#     conn=MySQLdb.connect(host='localhost',user='root',passwd='',db='renren_data',port=3306)
+#     cur=conn.cursor()
+#     cur.execute('select * from test')
+#     a = cur.fetchmany()
+#     print a[0][0]
+#     cur.close()
+#     conn.close()
+# except MySQLdb.Error,e:
+#      print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+def get_conn():
+    try:
+        return MySQLdb.connect(host='localhost',user='root',passwd='',db='renren_data',port=3306)
+    except MySQLdb.Error,e:
+         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+def insert(form, row):
+    try:
+        conn=get_conn()
+        cur=conn.cursor()
+        sql = 'insert into '+form+' ('+','.join(row.keys())+') values ('+','.join(['%s' for x in row])+')'
+        cur.execute(sql, row.values())
+        conn.commit()
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+def insert_on_duplicate(form, row):
+    try:
+        conn=get_conn()
+        cur=conn.cursor()
+        sql = 'insert into '+form+' ('+','.join(row.keys())+') values ('+','.join(['%s' for x in row])+') ON DUPLICATE KEY UPDATE '+','.join([x+'=%s' for x in row.keys()])
+        cur.execute(sql, row.values()+row.values())
+        conn.commit()
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
