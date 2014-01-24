2c62550ad9099f8ca6948ed2a298f08c389d9f35
picasso250 <cumt.xiaochi@gmail.com>
2014-01-25 01:22:53 +0800

    big loop

--- fetch-info.py
+++ fetch-info.py
@@ -15,35 +15,50 @@ def get_or_create_school(name):
         print 'insert'
         return db.insert('school', {'name': name})
 
-
 def get_info(uid):
-    pass
-    t = fetch.get_url('www.renren.com', '/'+uid+'/profile', {'v': 'info_timeline'})
+    t = fetch.get_url('www.renren.com', '/'+str(uid)+'/profile', {'v': 'info_timeline'})
     info = parser.get_info(t)
-    # for x in info:
-    #     print x, info[x]
-    # print
+    for x in info:
+        print x, info[x]
+    print
     return info
 
 def save_info(uid, info):
-    pass
+
     # uni
-    info['uni_id'] = get_or_create_school(info['uni'])
-    del info['uni']
+    if info.has_key('uni_id'):
+        print 'no key uni_id'
+        info['uni_id'] = get_or_create_school(info['uni'])
+        del info['uni']
 
     # college
-    info['uni_id'] = get_or_create_school(info['college'])
-    del info['college']
+    if info.has_key('college_id'):
+        print 'no key college_id'
+        info['college_id'] = get_or_create_school(info['college'])
+        del info['college']
 
     # school
-    info['uni_id'] = get_or_create_school(info['high_scool'])
-    del info['high_scool']
+    if info.has_key('high_school_id'):
+        print 'no key high_school_id'
+        info['high_school_id'] = get_or_create_school(info['high_scool'])
+        del info['high_scool']
 
     info['uid'] = uid
     info['has_visit_info'] = 1
 
     db.insert_on_duplicate('student', info)
 
-uid = '146539724'
-info = get_info(uid)
-save_info(uid, info)
+# uid = '146539724'
+
+while True:
+    uid = db.find_one_val('select uid from student where has_visit_info=0 limit 1')
+    print uid
+    if uid:
+        print '--------------------',uid
+        info = get_info(uid)
+        save_info(uid, info)
+        # break
+    else:
+        break
+
+    pass
