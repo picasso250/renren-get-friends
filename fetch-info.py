ee452924d67f513caa4464d03a88112ba4270d78
picasso250 <cumt.xiaochi@gmail.com>
2014-01-25 01:09:14 +0800

    2 function

--- fetch-info.py
+++ fetch-info.py
@@ -16,24 +16,34 @@ def get_or_create_school(name):
         return db.insert('school', {'name': name})
 
 
+def get_info(uid):
+    pass
+    t = fetch.get_url('www.renren.com', '/'+uid+'/profile', {'v': 'info_timeline'})
+    info = parser.get_info(t)
+    # for x in info:
+    #     print x, info[x]
+    # print
+    return info
+
+def save_info(uid, info):
+    pass
+    # uni
+    info['uni_id'] = get_or_create_school(info['uni'])
+    del info['uni']
+
+    # college
+    info['uni_id'] = get_or_create_school(info['college'])
+    del info['college']
+
+    # school
+    info['uni_id'] = get_or_create_school(info['high_scool'])
+    del info['high_scool']
+
+    info['uid'] = uid
+    info['has_visit_info'] = 1
+
+    db.insert_on_duplicate('student', info)
+
 uid = '146539724'
-t = fetch.get_url('www.renren.com', '/'+uid+'/profile', {'v': 'info_timeline'})
-info = parser.get_info(t)
-# for x in info:
-#     print x, info[x]
-# print
-# print info
-# uni
-info['uni_id'] = get_or_create_school(info['uni'])
-del info['uni']
-
-# college
-info['uni_id'] = get_or_create_school(info['college'])
-del info['college']
-
-# school
-info['uni_id'] = get_or_create_school(info['high_scool'])
-del info['high_scool']
-
-info['has_visit_info'] = 1
-print info
+info = get_info(uid)
+save_info(uid, info)
