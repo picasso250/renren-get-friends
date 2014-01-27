ff1156f63d880aeeb153c9ddeec120f4713d2eff
xiaochi <cumt.xiaochi@gmail.com>
2014-01-27 21:39:19 +0800

    is_delete

--- santi.sql
+++ santi.sql
@@ -5,9 +5,10 @@
 DROP TABLE IF EXISTS `scene`;
 CREATE TABLE `scene` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `number` int(6) DEFAULT NULL,
-  `number_extra` char(1) DEFAULT NULL,
+  `number` varchar(10) DEFAULT NULL,
+  `number_extra` char(6) DEFAULT NULL,
   `text` text,
   `img` varchar(255) DEFAULT NULL,
+  `is_delete` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
